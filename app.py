344ec5639bddd738a67c8ce707c03a46223b3194
Irisbek Joraboyev <irisbek@localhost.localdomain>
2024-12-07 00:03:29 +0500

    irisbek

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 # Modelni yuklash
-@st.cache
+
 def load_model():
-    with open('random_forest_model.pkl', 'rb') as file:
+    with open('random_f.pkl', 'rb') as file:
         return pickle.load(file)
 
 # Streamlit interfeysi
@@ -22,38 +22,19 @@ quantity = st.sidebar.number_input("Quantity", min_value=1, max_value=1000, valu
 unit_price = st.sidebar.number_input("UnitPrice", min_value=0.1, max_value=1000.0, value=5.0)
 customer_id = st.sidebar.number_input("CustomerID", min_value=1, max_value=50000, value=12345)
 
-# Ma'lumotlarni DataFrame ga aylantirish
-user_data = pd.DataFrame({
-    'Quantity': [quantity],
-    'UnitPrice': [unit_price],
-    'CustomerID': [customer_id]
-})
-
 # Modelni yuklash va bashorat qilish
 model = load_model()
 
 # Modelni ishlatish
 if st.button('Bashorat qilish'):
-    # 1. KMeans klasterlash
-    st.subheader("KMeans klasterlash natijalari")
     
-    # Normalizatsiya qilish
-    scaler = StandardScaler()
-    normalized_data = scaler.fit_transform(user_data[['Quantity', 'UnitPrice', 'CustomerID']])
-
-    # Agar faqat bitta qator bo'lsa, uni to'g'ri formatga keltirish
-    if normalized_data.shape[0] == 1:
-        normalized_data = normalized_data.reshape(1, -1)
-    
-    kmeans = KMeans(n_clusters=3, random_state=42)
-    kmeans.fit(normalized_data)  # Normallashtirilgan ma'lumotlar bilan fit qilish
-    
-    cluster = kmeans.predict(normalized_data)[0]
-    st.write(f"Sizning kiritingan ma'lumotlaringiz {cluster}-klasterga kiradi.")
-
     # 2. RandomForestClassifier yordamida bashorat qilish
     st.subheader("RandomForestClassifier yordamida bashorat qilish")
 
+    # Ma'lumotlarni NumPy arrayga o‘tkazish
+    input_data = np.array([quantity, unit_price, customer_id]).reshape(1, -1)
+
     # Bashorat qilish
-    y_pred = model.predict(user_data[['Quantity', 'UnitPrice', 'CustomerID']])
+    y_pred = model.predict(input_data)
     st.write(f"Bashorat qilingan qiymat: {y_pred[0]}")
+
