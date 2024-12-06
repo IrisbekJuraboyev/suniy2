2cbc390918af1b8175add1d6a540d4330ec8bf33
Irisbek <164008669+IrisbekJuraboyev@users.noreply.github.com>
2024-12-06 22:13:23 +0500

    Update modul.py

--- modul.py
+++ modul.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 import pickle
 
 # Modelni yuklash
@@ -22,14 +21,12 @@ st.sidebar.header("Ma'lumotlarni kiriting:")
 quantity = st.sidebar.number_input("Quantity", min_value=1, max_value=1000, value=10)
 unit_price = st.sidebar.number_input("UnitPrice", min_value=0.1, max_value=1000.0, value=5.0)
 customer_id = st.sidebar.number_input("CustomerID", min_value=1, max_value=50000, value=12345)
-country = st.sidebar.selectbox("Country", ["United Kingdom", "Germany", "France", "USA", "Australia"])
 
 # Ma'lumotlarni DataFrame ga aylantirish
 user_data = pd.DataFrame({
     'Quantity': [quantity],
     'UnitPrice': [unit_price],
-    'CustomerID': [customer_id],
-    'Country': [country]
+    'CustomerID': [customer_id]
 })
 
 # Modelni yuklash va bashorat qilish
@@ -50,15 +47,9 @@ if st.button('Bashorat qilish'):
     cluster = kmeans.predict(normalized_data)[0]
     st.write(f"Sizning kiritingan ma'lumotlaringiz {cluster}-klasterga kiradi.")
 
-    # 2. RandomForestClassifier bilan bashorat qilish
+    # 2. RandomForestClassifier yordamida bashorat qilish
     st.subheader("RandomForestClassifier yordamida bashorat qilish")
 
-    # Kodni ishlatish uchun Country nomini raqamli ko'rsatkichga aylantirish
-    country_mapping = {"United Kingdom": 1, "Germany": 2, "France": 3, "USA": 4, "Australia": 5}
-    user_data['Country'] = user_data['Country'].map(country_mapping)
-
     # Bashorat qilish
-    y_pred = model.predict(user_data[['Quantity', 'UnitPrice', 'CustomerID', 'Country']])
+    y_pred = model.predict(user_data[['Quantity', 'UnitPrice', 'CustomerID']])
     st.write(f"Bashorat qilingan qiymat: {y_pred[0]}")
-
-# Streamlit ilovasini ishga tushirish
